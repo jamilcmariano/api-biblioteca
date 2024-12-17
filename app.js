20cc3710d0bd008213599cb381ca07c122c2331b
jamilcmariano <jamilc.mariano@gmail.com>
2024-12-16 23:45:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Configuración de CORS
 app.use(cors({
   origin: [
     'http://localhost:5173',
     'https://frontend-biblioteca-eight.vercel.app'
-  ]
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+// Middleware para parsear JSON
 app.use(express.json());
+
+// Rutas
 app.use('/', routes);
 
+// Manejo básico de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('¡Algo salió mal!');
+});
+
+// Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
+module.exports = app;
