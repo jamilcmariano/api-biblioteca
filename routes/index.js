3b771b50a074375624654f9da494e8765c0ccb15
jamilcmariano <jamilc.mariano@gmail.com>
2024-12-16 23:42:41 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,13 @@
 const express = require('express');
-const cors = require('cors');
+const router = express.Router();
 const Biblioteca = require('../controllers/Biblioteca');
 
-const app = express();
-
-// Configurar CORS antes de las rutas
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://frontend-biblioteca-eight.vercel.app'
-  ]
-}));
-
 // Rutas para autores
-app.get('/autores', Biblioteca.obtenerAutores);
+router.get('/autores', Biblioteca.obtenerAutores);
 
 // Rutas para libros
-app.get('/libros', Biblioteca.obtenerLibros);
-app.get('/libros/disponibles', Biblioteca.obtenerLibrosDisponibles);
-app.get('/libros/nodisponibles', Biblioteca.obtenerLibrosNoDisponibles);
+router.get('/libros', Biblioteca.obtenerLibros);
+router.get('/libros/disponibles', Biblioteca.obtenerLibrosDisponibles);
+router.get('/libros/nodisponibles', Biblioteca.obtenerLibrosNoDisponibles);
 
-module.exports = app;
+module.exports = router;
