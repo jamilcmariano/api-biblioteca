f569753f314972c1511b5d3c82a9bf39fa181924
jamilcmariano <jamilc.mariano@gmail.com>
2024-12-16 23:43:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://frontend-biblioteca-eight.vercel.app'
+  ]
+}));
 
 app.use(express.json());
 app.use('/', routes);
